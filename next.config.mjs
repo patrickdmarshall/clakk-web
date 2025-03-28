9fa706771f2f1a3a24496028ddc4502f9882b50f
“Patrick <pmarshall.personal@gmail.com>
2025-03-28 12:51:35 -0700

    Set up Google Sheets API config and env vars

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+  },
+};
 
 export default nextConfig;
